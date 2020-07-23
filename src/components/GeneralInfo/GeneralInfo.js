86333c313e6749188e0b8f68e8cf5cb576fd194b
TitoGrine <tito999@gmail.com>
2020-07-23 22:24:53 +0100

    Started user preferences analysis.

--- src/components/GeneralInfo/GeneralInfo.js
+++ src/components/GeneralInfo/GeneralInfo.js
@@ -18,8 +18,47 @@ function GeneralInfo() {
 	const [ product, setProduct ] = useState('');
 	const [ link, setLink ] = useState('');
 	const [ uri, setURI ] = useState('');
+	const [ calcStats, setCalcStats ] = useState(true);
 
-	const refreshToken = () => {
+	const calcUserStats = (results) => {
+		spotifyWebApi.getAudioFeaturesForTracks(results).then(
+			function(data) {
+				console.log(data);
+			},
+			function(err) {
+				console.log(err);
+			}
+		);
+		console.log(results);
+	};
+
+	const getUserStats = () => {
+		spotifyWebApi
+			.getMyTopTracks({
+				limit: 50,
+				offset: 0,
+				time_range: 'long_term'
+			})
+			.then(
+				function(data) {
+					calcUserStats(
+						data.items.map((track) => {
+							return track.id;
+						})
+					);
+				},
+				function(err) {
+					console.log(err);
+					setCalcStats(false);
+
+					if (err.status == 401) refreshToken();
+
+					return;
+				}
+			);
+	};
+
+	const refreshToken = async () => {
 		const headers = {
 			refresh_token: sessionStorage.getItem('refreshToken')
 		};
@@ -39,9 +78,11 @@ function GeneralInfo() {
 		if (authToken && user === '') {
 			spotifyWebApi.setAccessToken(authToken);
 
+			getUserStats();
+
 			spotifyWebApi.getMe().then(
 				function(data) {
-                    console.log(data);
+					console.log(data);
 
 					setUser(data.display_name);
 					setImage(data.images[0].url);
@@ -97,7 +138,7 @@ function GeneralInfo() {
 							</li>
 							<li>
 								{' '}
-								<strong>Spotify link:</strong> {link}
+								<strong>Spotify link:</strong> <a href={link}>{link}</a>
 							</li>
 							<li>
 								{' '}
@@ -108,66 +149,61 @@ function GeneralInfo() {
 				</div>
 			</section>
 			<section className="sidebar-section slide-in-right">
-			<div className="side-content">
-						<Tabs>
-							<TabList>
-								<Tab>Settings</Tab>
-								<Tab>Go to</Tab>
-							</TabList>
+				<div className="side-content">
+					<Tabs>
+						<TabList>
+							<Tab>Settings</Tab>
+							<Tab>Go to</Tab>
+						</TabList>
 
-							<TabPanel>
-								<div className="settings">
-									<form
-										onChange={(ev) => {
-											console.log(ev.target.value);
-										}}
-									>
-										<p> Select a time range: </p>
-										<div className="time-labels">
-											<label>
-												<input
-													id="short_term"
-													type="radio"
-													name="radios"
-													value="short_term"
-													defaultChecked
-												/>
-												<span className="checkmark" />
-												Short Term
-											</label>
-											<label>
-												<input
-													id="medium_term"
-													type="radio"
-													name="radios"
-													value="medium_term"
-												/>
-												<span className="checkmark" />
-												Medium Term
-											</label>
-											<label>
-												<input id="long_term" type="radio" name="radios" value="long_term" />
-												<span className="checkmark" />
-												Long Term
-											</label>
-										</div>
-									</form>
-								</div>
-							</TabPanel>
-							<TabPanel>
-								<ul className="redirects">
-									<li>
-										{' '}
-										<Link to="/top">Top</Link>{' '}
-									</li>
-									<li>
-										{' '}
-										<Link to="/">Playlists</Link>{' '}
-									</li>
-								</ul>
-							</TabPanel>
-						</Tabs>
-					</div>
+						<TabPanel>
+							<div className="settings">
+								<form
+									onChange={(ev) => {
+										console.log(ev.target.value);
+									}}
+								>
+									<p> Select a time range: </p>
+									<div className="time-labels">
+										<label>
+											<input
+												id="short_term"
+												type="radio"
+												name="radios"
+												value="short_term"
+												defaultChecked
+											/>
+											<span className="checkmark" />
+											Short Term
+										</label>
+										<label>
+											<input id="medium_term" type="radio" name="radios" value="medium_term" />
+											<span className="checkmark" />
+											Medium Term
+										</label>
+										<label>
+											<input id="long_term" type="radio" name="radios" value="long_term" />
+											<span className="checkmark" />
+											Long Term
+										</label>
+									</div>
+								</form>
+							</div>
+						</TabPanel>
+						<TabPanel>
+							<ul className="redirects">
+								<li>
+									{' '}
+									<Link to="/top">Top</Link>{' '}
+								</li>
+								<li>
+									{' '}
+									<Link to="/">Playlists</Link>{' '}
+								</li>
+							</ul>
+						</TabPanel>
+					</Tabs>
+				</div>
 			</section>
 		</div>
 	);
